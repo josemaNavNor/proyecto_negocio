2d60b72a340e16d0c53b3e7f81c9d470442c3ba1
kevin Urquijo Luzania <luzania451147@gmail.com>
2024-10-14 16:26:44 -0700

    Cambios al nav, papeleria e icono de la pagina para regresar al catalogo inicial

--- pages/papeleria.jsx
+++ pages/papeleria.jsx
@@ -16,7 +16,40 @@ export default function Invitaciones() {
             </Layout>
 
             <LayoutProducto nombreCategoria="Papeleria">
-                <h2>No supe que poner crack</h2>
+                
+                <div className={styles.gridContainer}>
+                    {/* Animales */}
+                    <div className={styles.contenedor}>
+                        <h3 className={styles.h3}>Animales</h3>
+                        <div className={styles.imagenProducto}>
+                            <Image
+                                src="/img/papeleria/papMariposa.jpg"
+                                alt="Papeleria con animales"
+                                width={170}
+                                height={200}
+                            />
+                        </div>
+                        <Link href="/">
+                            <button className={styles.button}>Ver más</button>
+                        </Link>
+                    </div>
+
+                    {/* Papeleria con forma de textos */}
+                    <div className={styles.contenedor}>
+                        <h3 className={styles.h3}>Textos</h3>
+                        <div className={styles.imagenProducto}>
+                            <Image
+                                src="/img/papeleria/papAna.jpg"
+                                alt="Papeleria con textos o nombres"
+                                width={170}
+                                height={200}
+                            />
+                        </div>
+                        <Link href="/">
+                            <button className={styles.button}>Ver más</button>
+                        </Link>
+                    </div>
+                </div>
             </LayoutProducto>
         </>
     );
